e48ab70ec28cda3f74d4c0796d1e499260288590
fitsum belayneh <fitsu_alt@yahoo.com>
2017-12-09 12:34:20 -0500

    deploy

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ var mysql = require('mysql');
 // Create the MySQL connection object
 var connection;
 
-if (process.env.) {
+if (process.env.JAWSDB_URL) {
 	// DB is JawsDB on Heroku
-	connection = mysql.createConnection(process.env.);
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	// DB is local on localhost
 	connection = mysql.createConnection({
